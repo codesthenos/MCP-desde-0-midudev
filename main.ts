c76d660a37c39f0d05aaf01b123b5847ff9d1a4b
codesthenos <codesthenos@gmail.com>
2025-04-21 00:01:30 +0200

    Implement weather fetching functionality in Fecth-weather tool

--- main.ts
+++ main.ts
@@ -19,9 +19,27 @@ mcpServer.tool(
     city: z.string().describe("City to fetch the weather for"),
   },
   // funcion que se va a ejecutar cuando se llame a la herramienta, responde un json
-  async ({ city }) => ({
-    content: [{ type: "text", text: `The weather in ${city} is sunny` }],
-  })
+  async ({ city }) => {
+    const response = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`
+    );
+    const data = await response.json();
+
+    if (data.results.length === 0) {
+      return {
+        content: [{ type: "text", text: `No results found for ${city}` }],
+      };
+    }
+
+    const { latitude, longitude } = data.results[0];
+    const weatherResponse = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=temperature_2m,precipitation,is_day,rain&forecast_days=1`
+    );
+    const weatherData = await weatherResponse.json();
+    return {
+      content: [{ type: "text", text: JSON.stringify(weatherData) }],
+    };
+  }
 );
 
 // 3. Escuchar las conexiones de los clientes (le indicamos que no se conecte a internet, que todo sucede localmente)
